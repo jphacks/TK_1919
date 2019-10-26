f591bf4930dcc05e16b2543acfbb77419778ebab
NodaRyohei <ryoheinod@gmail.com>
2019-10-27 08:51:05 +0900

    英語でできるか試してみた あと若干ミスってるところ直した

--- gooAPIkeyword.py
+++ gooAPIkeyword.py
@@ -1,6 +1,6 @@
 
 import json
-from botocore.vendored import requests #AWS用
+#from botocore.vendored import requests #AWS用
 import requests #AWS以外用
 
 method = "POST"
@@ -18,14 +18,14 @@ def gooAPIkeyword(s_title,s_body,max_num) :
     params = {}
     headers = {'Content-Type' : 'application/json'}
     r = requests.post(url, params=params, data=json_data, headers=headers, proxies=proxies, timeout=10)
-    j = json.loads(r.text.decode('utf-8'))
+    #j = json.loads(r.text.decode('utf-8'))
 
-    return j
+    return r.text
 
 
 if __name__ == "__main__":
-    
-    j = gooAPIkeyword( "「和」をコンセプトとする 匿名性コミュニケーションサービス「MURA」 gooラボでのβ版のトライアル実施 ～gooの検索技術を使った「ネタ枯れ防止機能」によりコミュニティの話題活性化が可能に～" , "NTTレゾナント株式会社（本社：東京都港区、代表取締役社長：若井 昌宏、以下、NTTレゾナント）は、同じ興味関心を持つ人と匿名でコミュニティをつくることができるコミュニケーションサービス「MURA」を、2015年8月27日よりgooラボ上でβ版サイトのトライアル提供を開始します。" ,3)
+
+    j = gooAPIkeyword( "TWA85: 'The world's longest and most spectacular hijacking'" , "Under the hills of southern Italy, a little north-east of Naples, a fault ruptured and the earth began shaking. Those living on the surface, in one of the most earthquake-prone parts of Europe, were used to this. The 6.1-magnitude quake in the early evening was enough to frighten everyone, but it was the two powerful aftershocks that did the most damage.Twenty kilometres up from the epicentre and a few hundred metres north was where the Minichiello family lived, including 12-year-old Raffaele. By the time the third earthquake had subsided, their village of Melito Irpino was uninhabitable. The Minichiello family were left with nothing, Raffaele would later recall, and no-one in authority came to help.The damage was such that almost the entire village was evacuated, razed and rebuilt. Many families would return, but the Minichiellos decided to move to the US for a better life.What Raffaele Minichiello found instead was war, trauma and notoriety." ,3)
 
     #k = j["keywords"]
     #l = k[0]
