a43e3d571933dc4bfa488299dc0ae283714dd3f2
Keiya kaneko <kiyakaneko2845@gmail.com>
2019-10-26 17:50:54 +0900

    なんかあってそう. ソートまで実装した

--- choose_bestmatch.py
+++ choose_bestmatch.py
@@ -1,29 +1,24 @@
 import sys
 import math
-def sim_distance(prefs, person1, person2):
-    # person1とperson2が共に評価してるもののリスト
-    si = {}
-
-    for item in prefs[person1]:
-        if item in prefs[person2]:
-            si[item] = 1
-
-    # person1とperson2がどちらも評価してるものが無ければ類似性は0
-    if len(si) == 0 :
-        return 0
-
-    # 各項目ごとの差の平方
-    squares = [(prefs[person1][item] - prefs[person2][item]) ** 2 for item in si]
-    sum_of_sqrt = math.sqrt(sum(squares))
-    return 1/(1 + sum_of_sqrt)
+def sim_distance(user,city):
+  sum_sqrt = 0
+  for i in range(len(user)):
+    sum_sqrt += (user[i] - city[i]) ** 2
+  return 1/(1+sum_sqrt)
 
 def tsv_input():
   args = sys.argv
   city_file = args[1]
   user_preference = args[2]
+  with open(user_preference) as f:
+    line = f.readline()
+    user = list(map(int,line.split()))
+    print(user)
   with open(city_file) as f:
     line = f.readline()
     cnt = 0
+    sum_sqrt_list = []
+    city = []
     while line:
       if cnt == 0:
         name_eval = list(map(str,line.split()))
@@ -31,15 +26,15 @@ def tsv_input():
         line = f.readline()
         pass # 先頭行
       cnt += 1
-      #print(list(map(str,line.split())))
       city_info = list(map(str,line.split()))
       city_name = list(map(str,line.split()))[0]
-      city_val = list(map(str,line.split()))[1:]
-      city_dict = dict(zip(name_eval,city_val))
-      print(city_dict)
+      city_val = [int(s) for s in city_info[1:]]
+      #print(city_val)
+      sum_sqrt_list.append(sim_distance(user,city_val))
+      city.append(city_name)
       #print(preference_list)
       line = f.readline()
-
+    print(sorted(zip(sum_sqrt_list,city)))
 if __name__ == "__main__":
   tsv_input()
 
