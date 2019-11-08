31a17f8b24f8bbdf6583fa9b966b9a8f64beea72
NodaRyohei <ryoheinod@gmail.com>
2019-11-09 02:47:54 +0900

    国を決めたら結果を一つとってきて金額とurlを表示してurlを開くようにした

--- skyscanner_sample.py
+++ skyscanner_sample.py
@@ -1,19 +1,36 @@
 import requests
+import webbrowser
+import datetime
 
 url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0"
 APIKey = "c92d3c4eecmshb37fe7ee2e6c5eap106f08jsn556a682992c3"
 
-payload = "country=US&currency=USD&locale=en-US&originPlace=SFO-sky&destinationPlace=LHR-sky&outboundDate=2019-12-01&adults=1"
+#payload = "country=US&currency=JPY&locale=en-US&originPlace=SFO-sky&destinationPlace=LHR-sky&outboundDate=2019-12-01&adults=1"
+
+country = "UK"
+airportdata = {"US" : "SFO","UK" : "LHR"}
+airport = airportdata[country]
+
+params={
+    "country": "JP",
+    "currency": "JPY",
+    "locale": "en-US",
+    "originPlace": "HND-sky",
+    "destinationPlace": airport+"-sky",
+    "outboundDate": str(datetime.date.today()),
+    "adults": 1
+}
+
 headers = {
     'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
     'x-rapidapi-key': APIKey,
     'content-type': "application/x-www-form-urlencoded"
     }
 
-response = requests.request("POST", url, data=payload, headers=headers)
+response = requests.request("POST", url, data=params, headers=headers)
 
-print(response.status_code)
-print(response.headers)
+#print(response.status_code)
+#print(response.headers)
 
 r = response.headers
 r = r['Location']
@@ -22,7 +39,7 @@ sessionkey = r[-1]
 
 url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/"+sessionkey
 
-querystring = {"pageIndex":"0","pageSize":"10"}
+querystring = {"pageIndex":"1","pageSize":"1"}
 
 headers = {
     'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
@@ -31,8 +48,16 @@ headers = {
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 
-path_w = 'skyscannerresult.txt'
-with open(path_w, mode='a') as f:
-    f.write(response.text)
-#print(url)
+response = response.json()
+r = response['Itineraries']
+r = r[0]
+r = r['PricingOptions']
+r = r[0]
+#print(r)
+price = r['Price']
+link = r['DeeplinkUrl']
+
+print(price,link)
+webbrowser.open(link,1)
+
 #print(response.text)
